939981f83ed50c03b3bf177cda6dd5224a6eccb9
Samuel Ivan <maztersamzz@gmail.com>
2024-03-31 10:28:35 +0700

    53 Backend About Page Setup Part 1

--- laravel-udemy-laravel10-website/routes/web.php
+++ laravel-udemy-laravel10-website/routes/web.php
@@ -4,6 +4,7 @@ use App\Http\Controllers\ProfileController;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\AdminController;
 use App\Http\Controllers\Home\HomeSliderController;
+use App\Http\Controllers\Home\AboutController;
 
 /*
 |--------------------------------------------------------------------------
@@ -20,6 +21,7 @@ Route::get('/', function () {
     return view('frontend.index');
 });
 
+// Admin All Controller
 Route::controller(AdminController::class)->group( function () {
     Route::get('/admin/logout', 'destroy')->name('admin.logout');
     Route::get('/admin/profile', 'Profile')->name('admin.profile');
@@ -30,9 +32,15 @@ Route::controller(AdminController::class)->group( function () {
     Route::post('/update/password', 'UpdatePassword')->name('update.password');
 });
 
+// Home Slide All Route
 Route::controller(HomeSliderController::class)->group( function () {
     Route::get('/home/slide', 'HomeSlider')->name('home.slide');
     Route::post('/update/slider', 'UpdateSlider')->name('update.slider');
+});
+
+// About Page All Route
+Route::controller(AboutController::class)->group( function () {
+    Route::get('/about/page', 'AboutPage')->name('about.page');
 });
 
 Route::get('/dashboard', function () {
