5b0a904e9147dec60874085e7dc1daab3d0b50eb
Samuel Ivan <maztersamzz@gmail.com>
2024-04-14 03:49:43 +0700

    73 Backend Blog Page Setup Part 1

---
+++ laravel-udemy-laravel10-website/app/Models/Blog.php
@@ -0,0 +1,39 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Spatie\Sluggable\HasSlug;
+use Spatie\Sluggable\SlugOptions;
+use Illuminate\Database\Eloquent\Model;
+
+class Blog extends Model
+{
+    use HasFactory;
+    use HasSlug;
+
+    protected $fillable = [
+        'blog_category_id',
+        'title',
+        'image',
+        'tags',
+        'description'
+    ];
+
+    public function getSlugOptions() : SlugOptions
+    {
+        return SlugOptions::create()
+            ->generateSlugsFrom('name')
+            ->saveSlugsTo('slug');
+    }
+
+    /**
+     * Get the route key for the model.
+     *
+     * @return string
+     */
+    public function getRouteKeyName()
+    {
+        return 'slug';
+    }
+}
