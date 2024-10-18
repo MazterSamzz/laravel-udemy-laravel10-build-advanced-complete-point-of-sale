25571cf1cb4f3d74743fff21100670a989017638
Jessica's Acer <8948689+MazterSamzz@users.noreply.github.com>
2024-10-18 15:32:54 +0700

    Snyc Delivery Status and ERD PUML

--- POS/app/Enums/deliveryStatus.php
+++ POS/app/Enums/deliveryStatus.php
@@ -6,9 +6,9 @@ enum DeliveryStatus: int
 {
     case Pending = 1;            // Created by customer
     case Confirmed = 2;          // Confirmed by admin
-    case PartiallyShipped = 3;   // Partially shipped
-    case PartiallyDelivered = 4; // Partially delivered
-    case Shipped = 5;            // Fully Shipped
-    case Canceled = 6;           // Canceled order
+    case Canceled = 3;           // Canceled order
+    case PartiallyShipped = 4;   // Partially shipped
+    case PartiallyDelivered = 5; // Partially delivered
+    case Shipped = 6;            // Fully Shipped
     case Delivered = 7;          // Fully Delivered
 }
